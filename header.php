61a06fc0b4128b8938deabd50ccb05ecc6e3550f
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-10 00:16:36 -0500

    Added partial for mobile logo and inserted it into template.

--- header.php
+++ header.php
@@ -36,9 +36,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="title-bar" data-responsive-toggle="site-navigation">
 			<button class="menu-icon" type="button" data-toggle="offCanvas"></button>
-			<div class="title-bar-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			</div>
+            <?php get_template_part( '/parts/logo-mobile' ); ?>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
--- parts/logo-mobile.php
+++ parts/logo-mobile.php
@@ -1,8 +1,9 @@
 <?php
-// Inserts the site logo into the header navigation menu for DESKTOP
+// Inserts the site logo into the header navigation menu for MOBILE
+// Remember to optimize this image for mobile viewing
 ?>
 
-<div id="logo" class="home show-for-medium">
+<div id="logo" class="home">
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/virginian/fpo-logo.png" alt="The Virginian logo" title="The Virginian logo" width="993" height="549" />
     </a>
