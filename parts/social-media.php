e432052890961dd5f8d09b45748af4194f1eee82
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-10 01:08:23 -0500

    Updated social media links to target a fresh tab.

--- parts/social-media.php
+++ parts/social-media.php
@@ -5,13 +5,13 @@
 <div id="social-media" class="social-media show-for-medium">
     <ul>
         <li>
-            <a href="http://#" class="fa fa-facebook"></a>
+            <a href="http://#" target="_blank" class="fa fa-facebook"></a>
         </li>
         <li>
-            <a href="http://#" class="fa fa-twitter"></a>
+            <a href="http://#" target="_blank" class="fa fa-twitter"></a>
         </li>
         <li>
-            <a href="http://#" class="fa fa-instagram"></a>
+            <a href="http://#" target="_blank" class="fa fa-instagram"></a>
         </li>
     </ul>
 </div>
