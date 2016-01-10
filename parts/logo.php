fce7752954dde4df4b70ef044f9446d832d8234e
Lori Gildersleeve <lgilders@Loris-iMac.local>
2016-01-09 23:47:15 -0500

    Added partial for logo and insert fpo logo image.

---
+++ parts/logo.php
@@ -0,0 +1,9 @@
+<?php
+// Inserts the site logo into the header navigation menu
+?>
+
+<div id="logo" class="home">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/virginian/fpo-logo.png" alt="The Virginian logo" title="The Virginian logo" width="993" height="549" />
+    </a>
+</div>
