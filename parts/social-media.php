a96e09cc73769de97afc1db18d467df1cb003962
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-10 22:49:38 -0500

    Updated links in social media partial.

--- parts/social-media.php
+++ parts/social-media.php
@@ -5,13 +5,13 @@
 <div id="social-media" class="social-media show-for-medium">
     <ul>
         <li>
-            <a href="http://#" target="_blank" class="fa fa-facebook"></a>
+            <a href="http://twitter.com/virginiangolf" target="_blank" class="fa fa-twitter"></a>
         </li>
         <li>
-            <a href="http://#" target="_blank" class="fa fa-twitter"></a>
+            <a href="http://facebook.com/pages/The-Virginian-Golf-Club/195025446409" target="_blank" class="fa fa-facebook"></a>
         </li>
         <li>
-            <a href="http://#" target="_blank" class="fa fa-instagram"></a>
+            <a href="http://instagram.com/thevirginiangolfclub" target="_blank" class="fa fa-instagram"></a>
         </li>
     </ul>
 </div>
