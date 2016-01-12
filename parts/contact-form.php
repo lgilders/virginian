ce9bff36c7355a24f90924317f1fc1837c304d35
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-11 22:24:29 -0500

    Working on mobile styles for contact form and mobile menu.

--- parts/contact-form.php
+++ parts/contact-form.php
@@ -3,7 +3,7 @@
 ?>
 
 <div id="contact-form-header">
-    <h3>Contact Us</h3>
+    <h5>Contact Us</h5>
     <p>Lorem ipsum dolor sit amet, ridens nemore iisque ex sea. Decore tibique gloriatur at quo, veri definitiones cu sea.
         Eum ne laudem mucius, ut vel omnis summo. Eum aperiri fabulas omnesque ad, nobis facete disputationi mea no.
         Ad est wisi sale, usu aliquid feugait no.</p>
