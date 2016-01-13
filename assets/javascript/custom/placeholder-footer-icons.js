04dc0d758d7e7e32ae5309228e81d4635979d1f7
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-12 21:31:16 -0500

    Added placeholders for footer images.

---
+++ assets/javascript/custom/placeholder-footer-icons.js
@@ -0,0 +1,7 @@
+;(function(window){
+    jQuery(document).ready(function() {
+        $('#menu-footer-primary li:nth-child(1)').addClass('fa fa-calendar');
+        $('#menu-footer-primary li:nth-child(2)').addClass('fa fa-camera');
+        $('#menu-footer-primary li:nth-child(3)').addClass('fa fa-newspaper-o');
+    });
+}(window));
