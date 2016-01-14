1697589b8cf1b23eadde83b4f33163dd63d6070e
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-13 21:07:24 -0500

    Create initial homepage template.

---
+++ templates/homepage.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Template Name: Homepage
+*/
+get_header(); ?>
+
+<header id="hero" role="banner">
+
+</header>
+
+<?php do_action( 'foundationpress_before_content' ); ?>
+
+<div id="highlights-primary">
+    <section id="real-estate">
+
+    </section>
+
+    <section id="discovery-visit">
+
+    </section>
+</div>
+
+<div id="center">
+
+</div>
+
+<div id="highlights-secondary">
+    <section id="golf-course">
+
+    </section>
+
+    <section id="the-club">
+
+    </section>
+</div>
+
+<?php do_action( 'foundationpress_after_content' ); ?>
+
+<!-- Insert Featured Property call here -->
+
+<?php get_footer(); ?>
