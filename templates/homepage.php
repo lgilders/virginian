8a6998e1f66d5050b3fa9eec66628714a8fd6337
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-13 21:44:22 -0500

    Added first pass of field calls in homepage template.

--- templates/homepage.php
+++ templates/homepage.php
@@ -4,33 +4,72 @@ Template Name: Homepage
 */
 get_header(); ?>
 
+<?php $heroImage = get_field( 'hero_image' ); ?>
 <header id="hero" role="banner">
+    <?php if($heroImage): ?>
+        <img src="<?php echo $heroImage['url']; ?>" />
+    <?php endif; ?>
 
+    <h1><?php echo get_field( 'hero_title' ); ?></h1>
+    <p><?php echo get_field( 'hero_introduction' ); ?></p>
 </header>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 
 <div id="highlights-primary">
+    <?php $firstLeftImage = get_field( 'first_left_image' ); ?>
     <section id="real-estate">
+        <?php if($firstLeftImage): ?>
+            <img src="<?php echo $firstLeftImage['url']; ?>" />
+        <?php endif; ?>
 
+        <?php echo get_field( 'first_left_title' ); ?>
+        <?php echo get_field( 'first_left_description' ); ?>
+        <?php echo get_field( 'first_left_button' ); ?>
     </section>
 
+    <?php $firstRightImage = get_field( 'first_right_image' ); ?>
     <section id="discovery-visit">
+        <?php if($firstRightImage): ?>
+            <img src="<?php echo $firstRightImage['url']; ?>" />
+        <?php endif; ?>
 
+        <?php echo get_field( 'first_right_title' ); ?>
+        <?php echo get_field( 'first_right_description' ); ?>
+        <?php echo get_field( 'first_right_button' ); ?>
     </section>
 </div>
 
+<?php $centerImage = get_field( 'center_image' ); ?>
 <div id="center">
+    <?php if($centerImage): ?>
+        <img src="<?php echo $centerImage['url']; ?>" />
+    <?php endif; ?>
 
+    <?php echo get_field( 'center_title' ); ?>
 </div>
 
 <div id="highlights-secondary">
+    <?php $secondLeftImage = get_field( 'second_left_image' ); ?>
     <section id="golf-course">
+        <?php if($secondLeftImage): ?>
+            <img src="<?php echo $secondLeftImage['url']; ?>" />
+        <?php endif; ?>
 
+        <?php echo get_field( 'second_left_title' ); ?>
+        <?php echo get_field( 'second_left_description' ); ?>
+        <?php echo get_field( 'second_left_button' ); ?>
     </section>
 
+    <?php $secondRightImage = get_field( 'second_right_image' ); ?>
     <section id="the-club">
+        <?php if ($secondRightImage): ?>
+            <img src="<?php echo $secondRightImage['url']; ?>" />
+        <?php endif; ?>
 
+        <?php echo get_field( 'second_right_title' ); ?>
+        <?php echo get_field( 'second_right_description' ); ?>
+        <?php echo get_field( 'second_right_button' ); ?>
     </section>
 </div>
 
