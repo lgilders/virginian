276b91d8ef7e892782c9cec4475e7875f6df8354
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-18 20:16:19 -0500

    Added all fields to primary template.

--- templates/primary.php
+++ templates/primary.php
@@ -11,11 +11,25 @@ get_header(); ?>
         <?php endif; ?>>
         <h1><?php echo get_field( 'hero_title' ); ?></h1>
         <p><?php echo get_field( 'hero_introduction' ); ?></p>
+        <?php echo get_field( 'sub_navigation' ); ?>
     </header>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 
+<div id="first">
+    <section id="description"
+        <p><?php echo get_field( 'secondary_text' ); ?></p>
+    </section>
+</div>
 
+<?php $brandImage = get_field( 'brand_image' ); ?>
+<div id="second"
+    <?php if($brandImage): ?>
+        style="background: url(<?php echo $brandImage['url']; ?>)"
+    <?php endif; ?>>
+
+    <h2><?php echo get_field( 'brand_story' ); ?></h2>
+</div>
 
 <?php do_action( 'foundationpress_after_content' ); ?>
 
