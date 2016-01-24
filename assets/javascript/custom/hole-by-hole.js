5e8339179425cc68083cc395d83134900549f0a1
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-24 16:25:05 -0500

    Added video as a possibility for the hole-by-hole carousel.

--- assets/javascript/custom/hole-by-hole.js
+++ assets/javascript/custom/hole-by-hole.js
@@ -14,6 +14,34 @@ $(document).ready(function ($) {
         sync3.find(".owl-item").eq(0).addClass("synced");
     });
 
+    sync1.on('initialized.owl.carousel', function(){ reset_video_size(); });
+
+    sync1.on('resized.owl.carousel', function(){ reset_video_size(); });
+
+
+    // Video
+    function reset_video_size(video_width){
+        //better use jquery selectors: owl.items() and $(owl.items()) give problems, don't know why
+        var items = $('.owl-item:not([data-video])');
+        var videos = $('.owl-video-wrapper');
+        var v_height = 0;
+
+        //user-defined width ELSE, width from inline css (when owl.autoWidth == false),
+        //ELSE, computed innerwidth of the first element.
+        var v_width = (video_width) ? video_width : ((items.css('width') != 'auto') ? items.css('width') : items.innerWidth());
+
+        items.each(function(){
+            var h = $(this).innerHeight();
+            var hInfo = $('.hole-information').innerHeight();
+
+            if(h > v_height) v_height = h - hInfo;
+        });
+
+        //set both width and height
+        videos.css({ 'height':v_height, 'width':v_width });
+    }
+
+    // Updates selected tab styles
     function updateClassName(current) {
         // For tablet and desktop
         $(sync2).find(".synced").removeClass("synced");
@@ -23,11 +51,13 @@ $(document).ready(function ($) {
         $(sync3).find(".owl-item").eq(current).addClass("synced");
     }
 
+    // Initializes carousel for images/content
     sync1
         .owlCarousel({
             items: 1,
             nav: true,
-            dots: false
+            dots: false,
+            video: true
         })
         .on('changed.owl.carousel', function (e) {
             if (!flag) {
@@ -37,6 +67,7 @@ $(document).ready(function ($) {
             }
         });
 
+    // Initializes carousel for hole numbers
     sync2
         .owlCarousel({
             items: 18,
@@ -58,6 +89,7 @@ $(document).ready(function ($) {
             }
         });
 
+    // Initializes carousel for front/back 9
     sync3
         .owlCarousel({
             items: 2,
