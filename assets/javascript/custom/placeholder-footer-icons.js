c4d723bbc584ff9222340dac5845911b1b0d1b0d
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-01-30 18:34:55 -0500

    Added styleguide styles to footers.

--- assets/javascript/custom/placeholder-footer-icons.js
+++
@@ -1,7 +0,0 @@
-;(function(window){
-    jQuery(document).ready(function() {
-        $('#menu-footer-primary li:nth-child(1)').addClass('fa fa-calendar');
-        $('#menu-footer-primary li:nth-child(2)').addClass('fa fa-camera');
-        $('#menu-footer-primary li:nth-child(3)').addClass('fa fa-newspaper-o');
-    });
-}(window));
