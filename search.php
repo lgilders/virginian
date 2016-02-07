dbe7d787bda86c6a54694163b33819ae26e3909d
Lori Gildersleeve <lgildersleeve@gmail.com>
2016-02-06 21:13:03 -0500

    Added mobile real estate search for all affected pages.

--- search.php
+++ search.php
@@ -88,7 +88,39 @@ get_header(); ?>
 	<?php do_action( 'foundationpress_after_content' ); ?>
 
 	</div>
-    <?php dynamic_sidebar( 'search-property-widget' ); ?>
+
+    <div class="real-estate-search" data-responsive-toggle="search-form">
+        <h5>Real Estate Search</h5>
+        <button class="c-hamburger c-hamburger--htx c-hamburger-search" type="button" data-toggle="offCanvas">
+            <span>toggle menu</span>
+        </button>
+        <script>
+            (function() {
+
+                "use strict";
+
+                var toggles = document.querySelectorAll(".c-hamburger-search");
+
+                for (var i = toggles.length - 1; i >= 0; i--) {
+                    var toggle = toggles[i];
+                    toggleHandler(toggle);
+                };
+
+                function toggleHandler(toggle) {
+                    toggle.addEventListener( "click", function(e) {
+                        e.preventDefault();
+                        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+                    });
+                }
+
+            })();
+        </script>
+
+        <div id="search-form">
+            <?php dynamic_sidebar( 'search-property-widget' ); ?>
+        </div>
+    </div>
+
 </div>
 <?php dynamic_sidebar( 'featured-property-widgets' ); ?>
 <?php get_footer(); ?>
--- templates/real-estate.php
+++ templates/real-estate.php
@@ -16,11 +16,41 @@ get_header(); ?>
             <?php {yoast_breadcrumb( '<p id="breadcrumbs" ', '</p>' );} ?>
         <?php endif; ?>
 
-        <?php if( is_page('available-homes') ) { ?>
-            <?php dynamic_sidebar( 'search-home-widget' ); ?>
-        <?php } else { ?>
-            <?php dynamic_sidebar( 'search-lot-widget' ); ?>
-        <?php } ?>
+        <div class="real-estate-search" data-responsive-toggle="search-form">
+            <h5>Real Estate Search</h5>
+            <button class="c-hamburger c-hamburger--htx c-hamburger-search" type="button" data-toggle="offCanvas">
+                <span>toggle menu</span>
+            </button>
+            <script>
+                (function() {
+
+                    "use strict";
+
+                    var toggles = document.querySelectorAll(".c-hamburger-search");
+
+                    for (var i = toggles.length - 1; i >= 0; i--) {
+                        var toggle = toggles[i];
+                        toggleHandler(toggle);
+                    };
+
+                    function toggleHandler(toggle) {
+                        toggle.addEventListener( "click", function(e) {
+                            e.preventDefault();
+                            (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+                        });
+                    }
+
+                })();
+            </script>
+
+            <div id="search-form">
+                <?php if( is_page('available-homes') ) { ?>
+                    <?php dynamic_sidebar( 'search-home-widget' ); ?>
+                <?php } else { ?>
+                    <?php dynamic_sidebar( 'search-lot-widget' ); ?>
+                <?php } ?>
+            </div>
+        </div>
 
         <div class="entry-content">
             <?php the_content(); ?>
